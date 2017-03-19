7183396bbd638f115c795331802ddca0934224f5
baomq <baomq@BAOMQ>
2017-03-19 11:13:46 +0700

    Edit 1 by eclipse

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+	//Edit 1 by eclipse
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
