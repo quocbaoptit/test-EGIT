867174b9e5dcd263ce31bf109d1a2da7cc4c7b2b
quocbaoptit <quocbaoptit@gmail.com>
2017-03-19 11:15:32 +0700

    Edit 2 by github

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 	//Edit 1 by eclipse
+	//Edit 2 by github
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
